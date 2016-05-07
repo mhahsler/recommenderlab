09cf3b70dc632d88ad7052442574a01aeff0ed58
mhahsler <michael@hahsler.net>
2016-05-06 23:26:17 -0500

    fixed AR.

--- R/RECOM_BIN_AR.R
+++ R/RECOM_BIN_AR.R
@@ -1,70 +1,70 @@
 ## recommendations based on association rules
 
 .BIN_AR_param <- list(
-  support = 0.1, 
+  support = 0.1,
   confidence = 0.3,
   maxlen = 2,
   measure = "confidence",
-  verbose = FALSE, 
+  verbose = FALSE,
   decreasing = TRUE
 )
 
 BIN_AR <- function(data, parameter = NULL) {
-  
+
   ## parameters
   p <- .get_parameters(.BIN_AR_param, parameter)
-  
+
   data <- data@data
-  
-  rule_base <- apriori(data, 
-    parameter=list(support=p$support, confidence=p$confidence, 
+
+  rule_base <- apriori(data,
+    parameter=list(support=p$support, confidence=p$confidence,
       minlen=2, maxlen=p$maxlen), control=list(verbose=p$verbose))
-  
+
   ## additional measures
-  if(p$measure == "cxs") quality(rule_base) <- cbind(quality(rule_base), 
+  if(p$measure == "cxs") quality(rule_base) <- cbind(quality(rule_base),
     cxs = quality(rule_base)$confidence * quality(rule_base)$support)
-  
+
   if(!p$measure %in% names(quality(rule_base))) quality(rule_base) <-
     cbind(quality(rule_base), interestMeasure(rule_base, method = p$measure,
       transactions = data))
-  
+
   ## sort rule_base
   rule_base <- sort(rule_base, by = p$measure, decreasing=p$decreasing)
-  
+
   model <- c(list(
     description = "AR: rule base",
     rule_base = rule_base
-  ), p 
-  ) 
-  
-  
+  ), p
+  )
+
+
   predict <- function(model, newdata, n=10, data=NULL, ...) {
-    
+
     ## newdata are userid
     if(is.numeric(newdata)) {
       if(is.null(data) || !is(data, "ratingMatrix"))
         stop("If newdata is a user id then data needes to be the training dataset.")
-      newdata <- data[newdata,]
+      newdata <- data[newdata,, drop=FALSE]
     }
-    
+
     n <- as.integer(n)
     measure <- model$measure
-    
+
     reclist <- list()
-    m <- is.subset(lhs(rule_base), newdata@data)
+    m <- is.subset(lhs(model$rule_base), newdata@data)
     for(i in 1:nrow(newdata)) {
       recom <- head(unique(unlist(
-        LIST(rhs(sort(rule_base[m[,i]], by=measure)), 
+        LIST(rhs(sort(model$rule_base[m[,i]], by=measure)),
           decode=FALSE))), n)
-      
+
       reclist[[i]] <- if(!is.null(recom)) recom else numeric(0)
     }
 
     names(reclist) <- rownames(newdata)
-        
+
     new("topNList", items = reclist, itemLabels = colnames(newdata), n = n)
   }
-  
+
   ## construct recommender object
   new("Recommender", method = "AR", dataType = "binaryRatingMatrix",
     ntrain = nrow(data), model = model, predict = predict)
